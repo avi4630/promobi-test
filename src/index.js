c99d96804610490d7a74a5e122d11357009558f9
Avinash Mane <avimanepatil@gmail.com>
2020-12-05 15:19:52 +0530

    change router object

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
       <App />
-    </Router>,
+    </HashRouter>,
   document.getElementById('root')
 );
 
